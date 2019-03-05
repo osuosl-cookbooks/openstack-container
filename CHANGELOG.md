63b80791efc7712c8b05ac7f70f95444c08b2db7
Lance Albertson <lance@osuosl.org>
2019-03-04 17:14:45 -0800

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+openstack-container CHANGELOG
+=============================
+This file is used to list changes made in each version of the
+openstack-container cookbook.
+
+0.1.0
+-----
+- Initial release of openstack-container
+
---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name             'openstack-container'
+maintainer       'Oregon State University'
+maintainer_email 'chef@osuosl.org'
+license          'Apache-2.0'
+chef_version     '>= 12.18' if respond_to?(:chef_version)
+issues_url       'https://github.com/osuosl-cookbooks/openstack-container/issues'
+source_url       'https://github.com/osuosl-cookbooks/openstack-container'
+description      'Installs/Configures openstack-container'
+long_description 'Installs/Configures openstack-container'
+version          '0.1.0'
+
+supports         'centos', '~> 7.0'
