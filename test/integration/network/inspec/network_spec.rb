7f492d74bb472d6d12e11528994620da11ffc342
Lance Albertson <lance@osuosl.org>
2019-11-13 16:08:12 -0800

    Test kuryr-server is listening and also add fake eth1

--- test/integration/network/inspec/network_spec.rb
+++ test/integration/network/inspec/network_spec.rb
@@ -56,3 +56,8 @@ describe service 'kuryr-libnetwork.service' do
   it { should be_enabled }
   it { should be_running }
 end
+
+describe port(23750) do
+  it { should be_listening }
+  its('processes') { should include 'kuryr-server' }
+end
